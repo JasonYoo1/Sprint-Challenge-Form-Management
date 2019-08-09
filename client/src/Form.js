bba60b4a07e4ecef96aa62ae2a73b6ca96b053f4
JasonYoo1 <jyoojs@gmail.com>
2019-08-09 08:38:57 -0700

    created formik form

---
+++ client/src/Form.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Field, withFormik } from 'formik';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const FormComponent = (props) => {
+  console.log(props);
+  const { values, touched, errors, status, addUser } = props;
+  useEffect(() => {
+    if (status) {
+      addUser(status);
+    }
+  }, [status]);
+
+  return (
+    <Form>
+      {touched.username && errors.username && <p className="error">{errors.username}</p>}
+      <Field type="text" name="username" placeholder="username"/>
+      {touched.password && errors.password && <p className="error">{errors.password}</p>}
+      <Field type="password" name="password" placeholder=""/>
+      <button type="submit">Submit</button>
+    </Form>
+  );
+};
+
+const FormikForm = withFormik({
+  mapPropsToValues: ({ username, password }) => {
+    return {
+      username : username || "",
+      password: password || "",
+    };
+  },
+  validationSchema: yup.object().shape({
+    username: yup.string()
+      .required("Enter Username"),
+    password: yup.string()
+      .min(4, "Password must be atleast 4 characters long")
+      .required("Password is required"),
+  }),
+  handleSubmit: (values, { resetForm, setStatus, setErrors }) => {
+    console.log("Request");
+    if (values.email === "jyoojs@gmail.com") {
+      setErrors({ email: "Email is already taken" });
+    } else {
+      axios.post('http://localhost:5000/api/register', values)
+        .then(res => {
+          console.log(res);
+          setStatus(res);
+          resetForm();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+})(FormComponent);
+
+
+export default FormikForm;
